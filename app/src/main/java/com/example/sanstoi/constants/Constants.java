74e4e98a50df59ae19e6f59d8e214e460eb58928
Eopue <595127679@qq.com>
2017-06-04 00:27:14 +0800

    第一次提交

---
+++ app/src/main/java/com/example/sanstoi/constants/Constants.java
@@ -0,0 +1,10 @@
+package com.example.sanstoi.constants;
+
+/**
+ * Created by Sans toi on 2017/6/3.
+ */
+
+public interface Constants {
+    public static String url = "http://192.168.57.1:8088";
+    public static String doubleTest = "^[-+]?[0-9]*\\.?[0-9]+$";
+}
